ce41262f5496cd5c56ccfb4ed90c9eb50b39669d
23Aryansharma11 <129526491+23Aryansharma11@users.noreply.github.com>
2024-08-14 03:55:27 +0000

    add projects

--- constants/index.ts
+++ constants/index.ts
@@ -1,214 +1,235 @@
 export const Skill_data = [
-    {
-      skill_name: "Html 5",
-      Image: "/html.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Css",
-      Image: "/css.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Java Script",
-      Image: "/js.png",
-      width: 65,
-      height: 65,
-    },
-    {
-      skill_name: "Tailwind Css",
-      Image: "/tailwind.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "React",
-      Image: "/react.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Redux",
-      Image: "/redux.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Type Script",
-      Image: "/ts.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Next js 13",
-      Image: "/next.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Node js",
-      Image: "/node-js.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Mongo db",
-      Image: "/mongodb.png",
-      width: 40,
-      height: 40,
-    },
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Type Script",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+
+];
+
+export const Socials = [
+  {
+    name: "X",
+    src: "/twitter1.svg",
+    link: "https://twitter.com/23aryansharma11"
+  },
+  {
+    name: "Github",
+    src: "/githubblue.svg",
+    link: "https://github.com/23Aryansharma11"
+  },
+  {
+    name: "Linkedin",
+    src: "/linkedin.svg",
+    link: "https://www.linkedin.com/in/aryan-sharma-378971270/"
+  },
+];
+
+
+
+export const Frontend_skill = [
+  {
+    skill_name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Java Script",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Material UI",
+    Image: "/mui.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Type Script",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Astro",
+    Image: "/astro.svg",
+    width: 60,
+    height: 60,
+  },
   
-  ];
-  
-  export const Socials = [
-    {
-      name: "X",
-      src: "/twitter1.svg",
-      link: "https://twitter.com/23aryansharma11"
-    },
-    {
-      name: "Github",
-      src: "/githubblue.svg",
-      link: "https://github.com/23Aryansharma11"
-    },
-    {
-      name: "Linkedin",
-      src: "/linkedin.svg",
-      link: "https://www.linkedin.com/in/aryan-sharma-378971270/"
-    },
-  ];
-  
-  
-  
-  export const Frontend_skill = [
-    {
-      skill_name: "Html 5",
-      Image: "/html.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Css",
-      Image: "/css.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Java Script",
-      Image: "/js.png",
-      width: 65,
-      height: 65,
-    },
-    {
-      skill_name: "Tailwind Css",
-      Image: "/tailwind.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Material UI",
-      Image: "/mui.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "React",
-      Image: "/react.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Redux",
-      Image: "/redux.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Type Script",
-      Image: "/ts.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Next js 13",
-      Image: "/next.png",
-      width: 80,
-      height: 80,
-    },
-  ];
-  
-  export const Backend_skill = [
-    {
-      skill_name: "Node js",
-      Image: "/node-js.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Express js",
-      Image: "/express.png",
-      width: 80,
-      height: 80,
-    },
-    {
-      skill_name: "Mongo db",
-      Image: "/mongodb.png",
-      width: 40,
-      height: 40,
-    },
-    {
-      skill_name: "Fire base",
-      Image: "/Firebase.png",
-      width: 55,
-      height: 55,
-    },
-    {
-      skill_name: "Appwrite",
-      Image: "/appwrite.svg",
-      width: 55,
-      height: 55,
-    },
-  
-  ];
-  
-  export const Full_stack = [
-    
-  
-  ];
-  
-  
-  
-  export const Other_skill = [
-    {
-      skill_name: "Python",
-      Image: "/pythonlogo.svg",
-      width: 60,
-      height: 60,
-    },
-    {
-      skill_name: "Java",
-      Image: "/java.svg",
-      width: 60,
-      height: 60,
-    },
-    {
-      skill_name: "C",
-      Image: "/c.svg",
-      width: 60,
-      height: 60,
-    },
-    {
-      skill_name: "C++",
-      Image: "/c plusplus.svg",
-      width: 60,
-      height: 60,
-    },
-  ];
-  
-  
+];
+
+export const Backend_skill = [
+  {
+    skill_name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express js",
+    Image: "/express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Fire base",
+    Image: "/Firebase.png",
+    width: 55,
+    height: 55,
+  },
+  {
+    skill_name: "Appwrite",
+    Image: "/appwrite.svg",
+    width: 55,
+    height: 55,
+  },
+
+];
+
+export const Full_stack = [
+
+
+];
+
+
+
+export const Other_skill = [
+  {
+    skill_name: "Python",
+    Image: "/pythonlogo.svg",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "Java",
+    Image: "/java.svg",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "C",
+    Image: "/c.svg",
+    width: 60,
+    height: 60,
+  },
+  {
+    skill_name: "C++",
+    Image: "/c plusplus.svg",
+    width: 60,
+    height: 60,
+  },
+];
+
+
+
 export const projects = [
+  {
+    src: "/western-fiber.png",
+    title: "Western Fiber",
+    Description: "Developed a complete website for Western Fibers independently.",
+    link: "https://western-fiber.in/",
+    githubLink: "https://github.com/23Aryansharma11/Western-Fiber"
+  }, {
+    src: "/rightsinfoindia.png",
+    title: "Rights Info India",
+    Description: "A project aimed at educating people about their rights in India.",
+    link: "https://rights-info-india.netlify.app/",
+    githubLink: "https://github.com/23Aryansharma11/rightsInfoIndia"
+  },
   {
     src: "/google-clone.png",
     title: "Google Clone",
